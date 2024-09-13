676119d540248c18d157505ddeb4bfcfd62ea866
Zhandos Ali <108670939+prideofgiraffes@users.noreply.github.com>
2024-09-13 17:09:40 +0500

    Первый commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var username string = "Zhandos"
+	var password string = "Aliakbar"
+
+	fmt.Println("Authorization: Basic", username+" Luchshiy "+password)
+}
